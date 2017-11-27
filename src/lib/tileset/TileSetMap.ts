3dd2b50d882379133025232f5f821aff5d44b037
Daniel Peters <daniel.peters.ch@gmail.com>
2017-11-27 23:46:35 +0100

    Added tileset demo

---
+++ src/lib/tileset/TileSetMap.ts
@@ -0,0 +1,45 @@
+export class TileSetMap {
+  image
+  context
+  mapLayers
+  tileSize: number
+  tilesPerRow: number
+  tilesPerColumn: number
+  imageTilesPerRow: number
+
+  constructor (image, mapLayers, context, tileSize: number, tilesPerRow: number, tilesPerColumn: number, imageTilesPerRow: number) {
+    this.image = image
+    this.mapLayers = mapLayers
+    this.context = context
+    this.tileSize = tileSize
+    this.tilesPerRow = tilesPerRow
+    this.tilesPerColumn = tilesPerColumn
+    this.imageTilesPerRow = imageTilesPerRow
+  }
+
+  drawLayer (layer) {
+    for (let row = 0; row < this.imageTilesPerRow; row++) {
+      for (let col = 0; col < this.tilesPerColumn; col++) {
+        let tile = layer[row][col]
+        let tileRow = (tile / this.imageTilesPerRow) | 0
+        let tileCol = (tile % this.imageTilesPerRow) | 0
+        this.context.drawImage(
+          this.image,
+          (tileCol * this.tileSize),
+          (tileRow * this.tileSize),
+          this.tileSize,
+          this.tileSize,
+          (col * this.tileSize),
+          (row * this.tileSize),
+          this.tileSize,
+          this.tileSize
+        )
+      }
+    }
+  }
+
+  draw () {
+    this.mapLayers.forEach(layer => this.drawLayer(layer))
+
+  }
+}
